2fd43bb692139f80874c8db4169e514b564e7959
Luis <mluise2406@gmail.com>
2021-04-30 09:41:42 -0600

    Issues fixed

--- core/Calculator.py
+++ core/Calculator.py
@@ -12,25 +12,30 @@ class Calculator:
         if len(arr) < 3:
             if self.isValid(arr):
                 return arr[0] + arr[1]
-            return False
-        return self.add(arr[:-1]) + arr[-1]
+        elif isinstance(arr[-1], (int, float)):
+            return self.add(arr[:-1]) + arr[-1]
+        return False
 
     def substract(self,arr):
         if len(arr) < 3:
             if self.isValid(arr):
                 return arr[0] - arr[1]
-            return False
-        return self.substract(arr[:-1]) - arr[-1]
+        elif isinstance(arr[-1], (int, float)):
+            return self.substract(arr[:-1]) - arr[-1]
+        return False
 
     def multiply(self,arr):
         if len(arr) < 3:
             if self.isValid(arr):
                 return arr[0] * arr[1]
-            return False
-        return arr[-1] * self.multiply(arr[:-1])
+        elif isinstance(arr[-1], (int, float)):
+           return arr[-1] * self.multiply(arr[:-1])
+        return False
         
     
     def div(self, data):
         if self.isValid(data) and data[1] != 0:
             return data[0]/data[1]
         return False
+
+(Calculator()).add([5,3,'hwllo',3])
